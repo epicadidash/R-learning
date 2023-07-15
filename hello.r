e58d86efb22f1e4d767daa1999bf43736483ab30
epicadidash <dradwaitdash@gmail.com>
2023-07-16 01:08:45 +0530

    16.7.23 1 am

--- hello.r
+++ hello.r
@@ -1,4 +1,6 @@
-hello <- function() { 
-  print("Hello World!")
-}
-hello()
+#Vectors
+
+ vtr1 = c(TRUE, FALSE) #Logical
+ vtr2 = c(15,85.67494,9999999) #numeric
+ vtr3 = c(58L,56L) #integer
+ vtr4 = c("Hello", "World") #character
